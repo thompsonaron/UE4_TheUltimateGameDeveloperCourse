7a8ff160cbd03d0e7c8b3727c245936c86ad4265
AronT <aron.thompson@racunarstvo.hr>
2021-03-18 15:07:42 +0100

    Added basic movement pawn

---
+++ Source/FirstProject/Critter.cpp
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Critter.h"
+
+// Sets default values
+ACritter::ACritter()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+
+	// Postavljam root svog objekta? Mislim da ne. Mislim da spajam za root komponentu
+	MeshComponent->SetupAttachment(GetRootComponent());
+	// Ovo bi trebalo biti za postavljanje root komponente
+	//SetRootComponent(MeshComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(GetRootComponent());
+	Camera->SetRelativeLocation(FVector(-300.f, 0.f, 300.f));
+	Camera->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
+
+	// allows us to posses default pawn
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	CurrentVelocity = FVector(0.f);
+	MaxSpeed = 100.f;
+
+
+	// Example
+	//CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("MySpringArmComponent0"));
+	//CameraBoom->SetupAttachment(<Your character mesh or something>);
+
+}
+
+// Called when the game starts or when spawned
+void ACritter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACritter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+	SetActorLocation(NewLocation);
+}
+
+// Called to bind functionality to input
+void ACritter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// 1st parameter is which axis am I binding from Input, second is what obj am I binding it to, third is to what function am I sending it to - it will feeed axis value to the function?
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ACritter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ACritter::MoveRight);
+
+}
+
+void ACritter::MoveForward(float Value)
+{
+	CurrentVelocity.X = FMath::Clamp(Value, -1.f, 1.f) * MaxSpeed;
+}
+
+void ACritter::MoveRight(float Value)
+{
+	CurrentVelocity.Y = FMath::Clamp(Value, -1.f, 1.f) * MaxSpeed;
+}
---
+++ Source/FirstProject/Critter.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+//#include "Components/StaticMeshComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "Critter.generated.h"
+
+UCLASS()
+class FIRSTPROJECT_API ACritter : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACritter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MeshComponent;
+
+	// forward declaration example
+	// class UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(EditAnywhere)
+	class UCameraComponent* Camera;
+	
+	UPROPERTY(EditAnywhere, "Pawn Movement")
+	float MaxSpeed;
+
+private:
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+	FVector CurrentVelocity;
+
+};
